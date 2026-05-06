e8178babb30c0ff93fbdd779b206bfb1d5e8446c
Mariana Alves de Oliveira <marii2004.maa66@gmail.com>
2026-05-05 22:51:37 -0300

    Revert "Merge pull request #2 from Leodmv26/roberta"
    This reverts commit 8f19658a55e48d54cbc154874f011c474510a75f, reversing
    changes made to 201c7e7facb399ad5fd1b9418cd25f9914a7ba22.

--- projeto-SerraPet/mariana/js/contatos.js
+++
@@ -1,7 +0,0 @@
-function moverCachorro() {
-    var cachorro = document.getElementsByClassName("animacao")[0];
-    var posicao = Math.random() * (70 - 10) + 10;
-    cachorro.style.top = posicao + '%';
-}
-
-setInterval(moverCachorro, 6000);
